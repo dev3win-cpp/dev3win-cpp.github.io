c78e709c2a41443f8ce94fd65e1016095af7d8dc
Tadeusz <dev@3win.pl>
2023-02-24 19:12:22 +0100

    weather

--- weather/flutter_service_worker.js
+++ weather/flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "587a497a39477ac4d58da210d8517b41",
-"index.html": "8214172cccb8a2cba2e4de39899cd39e",
-"/": "8214172cccb8a2cba2e4de39899cd39e",
+"index.html": "01a72cf33704cdaf32fb5dca68790c1d",
+"/": "01a72cf33704cdaf32fb5dca68790c1d",
 "main.dart.js": "e154daf9a5f50edbfd3c131de2c853db",
+"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -18,7 +19,11 @@ const RESOURCES = {
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"assets/assets/sun.png": "1a2b8b308daa2564b82f512330f5e11b"
+"assets/assets/sun.png": "1a2b8b308daa2564b82f512330f5e11b",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
